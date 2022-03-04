a3d9ead91504f26c297849924f10cdb88b5f53b6
leyuri <leyuri97@gmail.com>
2022-03-04 22:42:53 +0900

    Add settings for the currently active scene

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 (() => {
+
+    let yOffset = 0; // window.pageYOffset 대신 쓸 변수 
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이 값의 합 
+    let currentScene = 0; // 현재 활성화된(눈앞에 보고 있는) scroll-section
+
     const sceneInfo = [
 
         {
@@ -8,7 +13,7 @@
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-0')
-            }
+            },
         },
         {
             // 1
@@ -17,7 +22,7 @@
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-1')
-            }
+            },
         },
         {
             // 2
@@ -26,7 +31,7 @@
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-2')
-            }
+            },
         },
         {
             // 3
@@ -35,7 +40,7 @@
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-3')
-            }
+            },
         },
     ]
 
@@ -45,9 +50,31 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
         }
-        console.log(sceneInfo)
     }
+
+    function scrollLoop() {
+        prevScrollHeight = 0
+        for (let i = 0; i < currentScene.length; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+        }
+
+        if (yOffset < prevScrollHeight) {
+            if (currentScene === 0) return
+            currentScene--;
+        }
+
+        console.log(prevScrollHeight)
+    }
+
     window.addEventListener('resize', setLayout)
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset
+        scrollLoop();
+    })
     setLayout()
 })()
 
